ef4eaab485425d55601e495498266ae2861c2cd0
Andriy_Boychuk <Andrii.boichuk.22@pnu.edu.ua>
2026-04-17 21:52:20 +0300

    add header

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
